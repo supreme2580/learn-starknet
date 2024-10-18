2a10745ed19ebddbe0805586eb12c134d63d714d
Supreme2580 <victoromorogbe69@gmail.com>
2024-10-18 14:57:09 +0100

    added extra utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,20 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function truncateWalletAddress(address: string) {
-  return `${address.slice(0, 6)}...${address.slice(-4)}`
+export function truncateWalletAddress(address: `0x${string}` | undefined) {
+  return `${address?.slice(0, 6)}...${address?.slice(-4)}`;
+}
+
+export function convertValue(amount: number, decimals: number) {
+  return (amount / 10 ** decimals).toFixed(4);
+}
+
+export function felt252ToString(felt: string | bigint): string {
+  let feltBigInt = BigInt(felt);
+  let result = '';
+  while (feltBigInt > 0) {
+    result = String.fromCharCode(Number(feltBigInt & BigInt(0xFF))) + result;
+    feltBigInt = BigInt(feltBigInt >> BigInt(8));
+  }
+  return result;
 }
